bf303148a052ede935d8d30150c3b36e98e8bed0
revoly afanasev <rewoly@mail.ru>
2026-01-04 17:27:40 +0900

    My first commit in 2026

--- src/main/java/com/example/nads/NetworkAnomalyDetectorApplication.java
+++ src/main/java/com/example/nads/NetworkAnomalyDetectorApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NetworkAnomalyDetectorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NetworkAnomalyDetectorApplication.class, args);
 	}
