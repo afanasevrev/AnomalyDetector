8da233ffec315202bd37fa9ea16e6d0d2a1b8be3
revoly afanasev <rewoly@mail.ru>
2026-01-04 17:12:34 +0900

    first project in 2026

---
+++ src/main/java/com/example/nads/NetworkAnomalyDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.nads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkAnomalyDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkAnomalyDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nads/NetworkAnomalyDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkAnomalyDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
